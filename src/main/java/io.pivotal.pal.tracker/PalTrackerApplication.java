818449b1d96e20826d7c63c48288e58138d88576
CarlosHerra <c.herraez47@gmail.com>
2020-09-23 12:20:41 +0000

    Fix datasource

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -15,9 +15,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setURL(System.getenv("SPRING_DATASOURCE_URL"));
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
         return new JdbcTimeEntryRepository(dataSource);
     }
 }
